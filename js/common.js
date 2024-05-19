7ab364aea8a480d5e0d0b91b8c41b41c0cbf7436
Suhyun Kim <kim00431@algonquinlive.com>
2024-05-18 22:14:24 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -23,3 +23,18 @@ const drop_down = ()=> {
 }
 
 drop_down();
+
+
+const header_scroll = ()=>{
+  const header = document.querySelector('.page_header');
+
+    window.addEventListener('scroll', () => {
+    if(window.scrollY > 200) {
+      header.classList.add('open')
+    }else {
+      header.classList.remove('open')
+    }
+  })
+}
+
+header_scroll();
