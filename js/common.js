51b9d194742a6c6ae5111620f5a9d06ff53ccca9
Suhyun Kim <kim00431@algonquinlive.com>
2024-06-21 01:15:10 -0400

    updates

--- js/common.js
+++ js/common.js
@@ -77,12 +77,3 @@ const faq = ()=>{
 header_scroll();
 faq();
 
-var video = $('.wrap-video').hover(hoverVideo, hideVideo);
-
-    function hoverVideo(e) {  
-        $('video', this).get(0).play(); 
-    }
-
-    function hideVideo(e) {
-        $('video', this).get(0).pause(); 
-}
